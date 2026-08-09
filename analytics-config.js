a60554cc061f8614c6607ca2bbe63cf6d41f4ecd
Amar Banerjee <amarbanerjee.ska@gmail.com>
2026-08-09 10:49:15 +0530

    Add privacy-aware analytics configuration

---
+++ analytics-config.js
@@ -0,0 +1,8 @@
+export default {
+  enabled: true,
+  endpoint: '',
+  privacyPage: 'privacy.html',
+  consentStorageKey: 'portfolio-analytics-consent-v1',
+  visitorStorageKey: 'portfolio-anon-visitor-v1',
+  sessionStorageKey: 'portfolio-anon-session-v1'
+};
